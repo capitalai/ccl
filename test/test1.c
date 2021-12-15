116c7a186dc31a633f86b02ea433064144c0eafd
Capita Lai <capita@gmail.com>
2021-12-16 01:46:59 +0800

    new test

--- test/test1.c
+++ test/test1.c
@@ -2,10 +2,17 @@
 #include "ccl/ccl.h"
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
-
-    printf("Hello, world!\n");
-
+int main(int argc, char *argv[]) {
+    arg_init();
+    arg_add_arg("file");
+    arg_load(argc, argv);
+    str *s = arg_arg("file");
+    if(s == NULL) {
+        printf("usage: test1 file\n");
+        arg_fini();
+        return 0;
+    }
+    printf("file = %s\n", str_data(s));
+    arg_fini();
     return 0;
-
 }
